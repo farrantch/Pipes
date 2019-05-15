7fcf7695f2e078055ec8198b0450254bc343fcfa
EC2 Default User <ec2-user@ip-172-31-82-11.ec2.internal>
2019-05-15 19:01:00 +0000

    generate master template from environments

--- buildspec/generate-master-template.py
+++ buildspec/generate-master-template.py
@@ -1,170 +1,66 @@
-import boto3
 import sys
 import time
 import os
 import json
-from botocore.exceptions import ClientError
+from collections import OrderedDict
 
 # Filenames
-file_scopes = 'Config-Scopes'
-file_cicd_parent = 'Scope-CICD-Parent'
-file_cicd_child = 'Scope-CICD-Child'
+file_environments = 'Config-Environments'
+file_master = 'Master'
 
 # Open Files
-# Input Files
-with open(file_scopes + '.template') as s_file:
-    scopes = json.load(s_file)
-with open('scope-templates/' + file_cicd_parent + '.template') as cp_file:
-    cicd_parent = json.load(cp_file)
-    
-client = boto3.client('cloudformation')
+with open(file_environments + '.template') as e_file:
+    environments = json.load(e_file, object_pairs_hook=OrderedDict)
+with open(file_master + '.template') as m_file:
+    master = json.load(m_file)
 
-#MASTERINFRASTACK = os.environ['MasterInfraStack']
-#ENVIRONMENT = os.environ['Environment']
-
-MASTERINFRASTACK = 'cicd-master-infra'
-ENVIRONMENT = 'cicd'
-
-# Get master infra stack resources
-aec = True
-try:
-    master_stack = client.describe_stacks(
-        StackName=MASTERINFRASTACK
-    )
-except ClientError:
-    aec = False
-    
-if aec:
-    resource_summaries = client.list_stack_resources(
-        StackName=MASTERINFRASTACK
-    )['StackResourceSummaries']
-    # Get existing child stack parameters
-    child_stack_parameters = {}
-    # Loop through master infra stack resources
-    for rs in resource_summaries:
-        # Only look for CloudFormation Stacks
-        if rs['ResourceType'] == 'AWS::CloudFormation::Stack':
-            # Get Stack parameters
-            child_stack_parameters[rs['LogicalResourceId']] = client.describe_stacks(
-                StackName = rs['PhysicalResourceId']
-            )['Stacks'][0]['Parameters']
-
-# Loop through infra scopes within pipeline file
-for key, value in scopes.items():
-    scope = key.lower()
-    # Determine AllEnvironmentsCreated value
-    if scope not in child_stack_parameters:
-        aec = False
-    if aec:
-        aec = list(filter(lambda item: item['ParameterKey'] == 'AllEnvironmentsCreated', child_stack_parameters[scope]))[0]['ParameterValue']
-    
-    # Insert Scope CICD Stack into Parent
-    cicd_parent['Resources'][scope] = {
-        "Type" : "AWS::CloudFormation::Stack",
-        "Properties": {
-            "Parameters": {
-                "DevAccount": {
-                    "Ref": "DevAccount"
-                },
-                "ProdAccount": {
-                    "Ref": "ProdAccount"
-                },
-                "MasterPipeline": {
-                    "Ref": "MasterPipeline"
-                },
-                "Environment": {
-                    "Ref": "Environment"
-                },
-                "Scope": scope,
-                "AllEnvironmentsCreated": str(aec),
-                "MasterS3BucketName": {
-                    "Fn::Sub": "${S3BucketName}"
-                }
-            },
-            "Tags" : [
+# Loop through environments
+for env in environments:
+    env_lower = env.lower()
+    master['Resources']['CodePipeline']['Properties']['Stages'].insert(-1,
+        {
+            "Name": env,
+            "Actions": [
                 {
-                    "Key": "Environment",
-                    "Value": {
-                        "Fn::Sub": "${Environment}"
-                    }
-                }
-            ],
-            "TemplateURL" : {
-                "Fn::Sub": "https://s3.amazonaws.com/${S3BucketName}/generated-cicd-templates/Scope-CICD-Child-" + scope + ".template"
-            }
-        }
-    }
-    
-    # Open child template to insert pipelines
-    with open('scope-templates/' + file_cicd_child + '.template') as cc_file:
-        cicd_child = json.load(cc_file)
-    
-    # Loop through pipelines
-    if 'Pipelines' in value:
-        for pipeline in value['Pipelines']:
-            name = pipeline['Name'].lower()
-            cicd_child['Resources'][pipeline['Name']] = {
-                "Type": "AWS::CloudFormation::Stack",
-                "Condition": "NotInitialCreation",
-                "DependsOn": ["RoleCodePipeline", "RoleCodeBuild"],
-                "Properties": {
-                    "Parameters": {
-                        "DevAccount": {
-                            "Ref": "DevAccount"
+                    "ActionTypeId": {
+                        "Category": "Deploy",
+                        "Owner": "AWS",
+                        "Provider": "CloudFormation",
+                        "Version": "1"
+                    },
+                    "Configuration": {
+                        "ActionMode": "CREATE_UPDATE",
+                        "Capabilities": "CAPABILITY_NAMED_IAM,CAPABILITY_AUTO_EXPAND",
+                        "RoleArn": {
+                            "Fn::Sub": "arn:aws:iam::${" + env + "Account}:role/" + env_lower + "-${AWS::StackName}-CloudFormationRole"
                         },
-                        "ProdAccount": {
-                            "Ref": "ProdAccount"
+                        "StackName": {
+                            "Fn::Sub": env_lower + "-${AWS::StackName}-infra"
                         },
-                        "MasterPipeline": {
-                            "Ref": "MasterPipeline"
-                        },
-                        "Scope": scope,
-                        "SubScope": name,
-                        "Environment": "cicd",
-                        "S3BucketName": {
-                            "Ref": "S3Bucket"
-                        },
-                        "KmsCmkArn": {
-                            "Fn::GetAtt": [
-                                "KmsKey",
-                                "Arn"
-                            ]
-                        },
-                        "IamRoleArnCodePipeline": {
-                            "Fn::GetAtt": [
-                                "RoleCodePipeline",
-                                "Arn"
-                            ]
-                        },
-                        "IamRoleArnCodeBuild": {
-                            "Fn::GetAtt": [
-                                "RoleCodeBuild",
-                                "Arn"
-                            ]
+                        "TemplatePath": "SDLCTemplates::Scope-SDLC-Parent.template",
+                        "TemplateConfiguration": "SourceOutput::cfvars/" + env + ".template",
+                        "ParameterOverrides": {
+                            "Fn::Sub": "{ \"S3BucketName\" : { \"Fn::GetArtifactAtt\" : [\"SourceOutput\", \"BucketName\"]}, \"CicdAccount\" : \"${AWS::AccountId}\", \"MasterPipeline\" : \"${AWS::StackName}\"}"
                         }
                     },
-                    "Tags": [{
-                        "Key": "Environment",
-                        "Value": {
-                            "Fn::Sub": "${Environment}"
+                    "Name": "DeployCloudFormation" + env,
+                    "InputArtifacts": [
+                        {
+                            "Name": "SourceOutput"
+                        },
+                        {
+                            "Name": "SDLCTemplates"
                         }
-                    }],
-                    "TemplateURL": {
-                        "Fn::Sub": "https://s3.amazonaws.com/${MasterS3BucketName}/infra-templates/CICD.template"
+                    ],
+                    "RunOrder": 1,
+                    "RoleArn": {
+                        "Fn::Sub": "arn:aws:iam::${" + env + "Account}:role/" + env_lower + "-${AWS::StackName}-CodePipelineRole"
                     }
                 }
-            }
-            
-            # Add Parameter Overrides
-            if 'Parameters' in pipeline:
-                for po, po_value in pipeline['Parameters'].items():
-                    # Filter to parameters only applicable to CICD stack
-                    if po == 'SdlcCodeBuildPre' or po == 'SdlcCodeBuildPost' or po == 'SdlcCloudFormation' or po == 'CfContainsLambda' or po == 'CicdCodeBuild' or po == 'CicdCodeBuildImage' or po == 'IncludeEnvCfTemplateConfigs':
-                        cicd_child['Resources'][pipeline['Name']]['Properties']['Parameters'][po] = po_value
-        
-    with open('generated-cicd-templates/' + file_cicd_child + '-' + scope + '.template', 'w') as cc_file_output:
-        json.dump(cicd_child, cc_file_output, indent=4)
+            ]
+        }
+    )
         
 # Save files
-with open('generated-cicd-templates/' + file_cicd_parent + '.template', 'w') as cp_file_output:
-    json.dump(cicd_parent, cp_file_output, indent=4)
+with open('generated-master-template/' + file_master + '.template', 'w') as master_file_output:
+    json.dump(master, master_file_output, indent=4)
